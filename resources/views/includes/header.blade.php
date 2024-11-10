8d896d8b72c73e5fe4a73065ad665b21e05ed81c
rnanayashiro <rnanayashiro@gmail.com>
2024-11-11 00:20:46 +0900

    WIP

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,5 @@
 <header id="header" class="header">
-    <h1 class="header-logo"><a href="/"><img src="/common/images/logo_corporate_white.png"
-                alt="dentsu tokyo/osaka/nagoya"></a></h1>
+    <h1 class="header-logo"><a href="/"><img src="/common/images/gccinc.svg" alt=""></a></h1>
     <button class="header-hamburger js-hamburger-button"><span class="header-hamburger-line"></span><span
             class="header-hamburger-line"></span><span class="header-hamburger-line"></span></button>
     <div class="jp-en-btn"><a href="/" class="jp-btn">JP</a>|<a class="en-btn -not-en" href="/en/">EN</a></div>
@@ -15,40 +14,6 @@
                     <div class="header-search-tags"><a href="/tag/">タグから探す</a></div>
                 </div>
                 <div class="header-contents">
-                    <div class="header-topics js-header-topics" data-effect="slide-up" data-effect-delay="14"
-                        data-effect-delay-sp="1">
-                        <p class="header-topics-title pc-content">Featured Content</p>
-                        <ul class="header-topics-box-list pc-content">
-                            <li>
-                                <a href="/aboutus/message.html" class="header-topics-box">
-                                    <div class="header-topics-box-pict"><img src="/aboutus/images/ceo_thumb_231208.jpg"
-                                            alt="トップメッセージ"></div>
-                                    <ul class="header-topics-breadcrumb">
-                                        <li>企業情報</li>
-                                    </ul>
-                                    <p class="header-topics-box-title"><span
-                                            style="font-size:1.2rem;">トップメッセージ</span><br>真のIntegrated Growth Partnerへ。
-                                    </p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/aboutus/de_and_i.html" class="header-topics-box">
-                                    <div class="header-topics-box-pict"><img
-                                            src="/aboutus/images/final_DE_I_for_each_page_01_230120.jpg" alt="">
-                                    </div>
-                                    <ul class="header-topics-breadcrumb">
-                                        <li>DEI</li>
-                                    </ul>
-                                    <p class="header-topics-box-title">
-                                        視点・論点の多様性を実現することでイノベーションを図り、継続的な事業成長を図る取り組みについてデータを用いてご紹介します。</p>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="header-topics-banner"><a href="/capabilities/"><img
-                                    src="/common/images/header_banner_item01.jpg" alt="" class="pc-content"><img
-                                    src="/common/images/header_banner_item01_sp.jpg" alt=""
-                                    class="sp-content"></a></div>
-                    </div>
                     <button class="header-dropdown-back-button js-dropdown-back-button">BACK</button>
                     <ul class="header-menu" data-effect-list="slide-left">
                         <li class="header-menu-item" data-effect-delay="6">
@@ -117,73 +82,8 @@
                                 </ul>
                             </div>
                         </li>
-                        <li class="header-menu-item" data-effect-delay="8"><a href="/showcase/"
-                                class="header-menu-title">ショーケース</a></li>
-                        <li class="header-menu-item" data-effect-delay="9"><a href="/labo/"
-                                class="header-menu-title">ラボ＆プロジェクト</a></li>
                         <li class="header-menu-item" data-effect-delay="10"><a href="/people/"
                                 class="header-menu-title">ピープル</a></li>
-                        <li class="header-menu-item" data-effect-delay="11"><a href="/knowledge/"
-                                class="header-menu-title header-dropdown-button js-dropdown-button">ナレッジ＆データ</a>
-                            <div class="header-dropdown -lv1 js-dropdown-menu">
-                                <p class="header-dropdown-title"><a href="/knowledge/">ナレッジ＆データ TOP</a></p>
-                                <ul class="header-dropdown-menu">
-                                    <li class="header-dropdown-menu-item">
-                                        <a href="/knowledge/ad_cost/"
-                                            class="header-dropdown-button js-dropdown-button">日本の広告費</a>
-                                        <div class="header-dropdown -lv2 js-dropdown-menu">
-                                            <p class="header-dropdown-title"><a href="/knowledge/ad_cost/">日本の広告費
-                                                    TOP</a></p>
-                                            <ul class="header-dropdown-menu">
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2023/">2023</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2022/">2022</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2021/">2021</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2020/">2020</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2019/">2019</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2018/">2018</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2017/">2017</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2016/">2016</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2015/">2015</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2014/">2014</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2013/">2013</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2012/">2012</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2011/">2011</a></li>
-                                                <li class="header-dropdown-menu-item"><a
-                                                        href="/knowledge/ad_cost/2010/">2010</a></li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="https://www.group.dentsu.com/jp/news/release/001206.html"
-                                            target="_blank">世界の広告費</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/knowledge/ad_amount.html">媒体別広告量</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/knowledge/publish/infomedia2024.html">情報メディア白書</a></li>
-                                    <li class="header-dropdown-menu-item"><a href="/knowledge/cmo_survey/">CMO調査</a>
-                                    </li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/knowledge/ad_nenpyo.html">広告景気年表</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/knowledge/summary/masscom.html">マスコミ功労者顕彰</a></li>
-                                    <li class="header-dropdown-menu-item"><a href="/knowledge/publish/">書籍・出版物</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
                         <li class="header-menu-item" data-effect-delay="12"><a href="/news/"
                                 class="header-menu-title header-dropdown-button js-dropdown-button">ニュースリリース</a>
                             <div class="header-dropdown -lv1 js-dropdown-menu">
@@ -235,66 +135,11 @@
                                 </ul>
                             </div>
                         </li>
-                        <li class="header-menu-item" data-effect-delay="13">
-                            <a href="/sustainability/"
-                                class="header-menu-title header-dropdown-button js-dropdown-button">サステナビリティ</a>
-                            <div class="header-dropdown -lv1 js-dropdown-menu">
-                                <p class="header-dropdown-title"><a href="/sustainability/">サステナビリティ TOP</a></p>
-                                <ul class="header-dropdown-menu">
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/sustainability/business_ethics_compliance_and_datasecurity/">企業倫理とコンプライアンス
-                                            / データセキュリティ</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/sustainability/human_capital_development/">人的資本の開発</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/sustainability/innovation_leadership/">イノベーションに導くリーダーシップ</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/sustainability/climateaction/">気候変動へのアクション</a></li>
-                                    <li class="header-dropdown-menu-item"><a href="/sustainability/dei/">DEI</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="https://www.group.dentsu.com/jp/sustainability/reports/"
-                                            target="_blank">統合レポート</a></li>
-                                    <li class="header-dropdown-menu-item"><a
-                                            href="/sustainability/covid19_social/">コロナ禍における電通の社会貢献活動</a></li>
-                                </ul>
-                            </div>
-                        </li>
                         <li class="header-menu-item" data-effect-delay="15"><a href="/careers/"
-                                class="header-menu-title">採用情報</a></li>
+                                class="header-menu-title">採用情報</a>
+                        </li>
                     </ul>
                 </div>
-            </div>
-            <div class="header-links-group" data-effect="fade-in" data-effect-delay="15">
-                <ul class="header-link-list -logo">
-                    <li><a href="https://dentsu-ho.com/?__hstc=201534478.aaae75e13abb92e813f3e91239c5182a.1729084990391.1730638934599.1730696341959.3&amp;__hssc=201534478.3.1730696341959&amp;__hsfp=4016365777"
-                            target="_blank"><img src="/common/images/logo_dentsu_ho.png" alt="電通報"></a></li>
-                    <li><a href="https://www.d-sol.jp/?__hstc=201534478.aaae75e13abb92e813f3e91239c5182a.1729084990391.1730638934599.1730696341959.3&amp;__hssc=201534478.3.1730696341959&amp;__hsfp=4016365777"
-                            target="_blank"><img src="/common/images/logo_d_sol.png" alt="Do! SOLUTIONS"></a></li>
-                    <li><a href="https://transformation-showcase.com/?__hstc=201534478.aaae75e13abb92e813f3e91239c5182a.1729084990391.1730638934599.1730696341959.3&amp;__hssc=201534478.3.1730696341959&amp;__hsfp=4016365777"
-                            target="_blank"><img src="/common/images/logo_tsc.png" alt="Transformation SHOWCASE"></a>
-                    </li>
-                    <li><a href="https://www.japan.dentsu.com/jp/" target="_blank"><img
-                                src="/common/images/logo_japan_dentsu_230101.png" alt="dentsu Japan"></a></li>
-                    <li><a href="https://www.dentsusoken.com/thinktank" target="_blank"><img
-                                src="/common/images/logo_institute_dentsu.png" alt="電通総研"></a></li>
-                    <li><a href="https://www.group.dentsu.com/" target="_blank"><img
-                                src="/common/images/logo_dentsu.png" alt="dentsu"></a></li>
-                </ul>
-            </div>
-            <div class="header-links" data-effect="fade-in" data-effect-delay="14">
-                <ul class="header-link-list">
-                    <li><a href="/terms/business_guideline.html">電通の事業に関する規約・ガイドライン</a></li>
-                    <li><a href="/terms/index.html">本サイトに関わる規約・ガイドライン</a></li>
-                    <li><a href="/terms/rss.html">RSSについて</a></li>
-                    <li><a href="/tag/">タグ一覧</a></li>
-                    <li><a href="/aboutus/jigyosho.html">マップ</a></li>
-                    <li><a href="/mail/">お問い合わせ</a></li>
-                </ul>
-                <ul class="social-list">
-                    <li class="social-list-item -sm"><a href="https://www.linkedin.com/company/dentsujapan/"
-                            target="_blank"><img src="/common/images/icon_social_linkedin_white.svg"
-                                alt="LinkedIn"></a></li>
-                </ul>
             </div>
         </div>
     </nav>
@@ -302,13 +147,13 @@
 @section('script')
     <script>
         $(document).ready(function() {
-            $('#menuToggle').click(function() {
+            // ハンバーガーメニューのクリックイベント
+            $('.js-hamburger-button').on('click', function() {
+                // メニューを開閉する
+                $('.header-nav').toggleClass('open'); // メニューに 'open' クラスを追加または削除
 
-                $('#sideMenu').addClass('open');
-            });
-
-            $('#closeMenu').click(function() {
-                $('#sideMenu').removeClass('open');
+                // ハンバーガーアイコンのアニメーション（例: 変形）
+                $(this).toggleClass('open'); // ハンバーガーアイコンの状態も変更
             });
         });
     </script>
