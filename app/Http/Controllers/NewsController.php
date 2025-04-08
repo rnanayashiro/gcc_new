a810458820d8662d07f3df4eb128eb47900e35ff
rnanayashiro <rnanayashiro@gmail.com>
2025-04-08 11:26:47 +0900

    newsdb

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\News;
+
+class NewsController extends Controller
+{
+    // フォーム画面表示
+    public function showForm()
+    {
+        return view('newsForm');
+    }
+
+
+    // フォーム送信時に実行されるメソッド
+    public function submit(Request $request)
+    {
+        // バリデーション
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',  // タイトルは必須、文字列、最大255文字
+            'date' => 'required|date',  // 日付は必須、date形式
+            'category' => 'required|string|max:255',  // カテゴリは必須、文字列、最大255文字
+            'content' => 'required|string',  // 本文は必須、文字列
+        ]);
+
+        // バリデーションが成功した場合にデータを保存
+        News::create([
+            'title' => $validated['title'],
+            'date' => $validated['date'],
+            'category' => $validated['category'],
+            'content' => $validated['content'],
+        ]);
+
+        // 登録後にフォームをリダイレクトして成功メッセージを表示
+        return redirect()->route('news')->with('success', 'ニュース記事が登録されました。');
+    }
+}
