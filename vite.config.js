62671a58ee2dea328aac5f15161f58cbd310a607
rnanayashiro <rnanayashiro@gmail.com>
2025-04-28 13:09:47 +0900

    header fix arrow

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/js/beta.js",
+                "resources/css/common.css",
+                "resources/css/pagesetting.css",
             ],
             refresh: true,
         }),
