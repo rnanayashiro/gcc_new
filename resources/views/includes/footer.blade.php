9efa66cc98e51ea63a6d69189f9a28480e0e6fba
rnanayashiro <rnanayashiro@gmail.com>
2025-06-22 21:27:34 +0900

    fix

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,9 +2,9 @@
     <div class="footer-inner">
         <div class="footer-links-wrap">
             <ul class="footer-links">
-                <li><a href="{{ route('sitePolicy') }}">{{ __('messages.site_policy') }}</a></li>
-                <li><a href="{{ route('privacyPolicy') }}">{{ __('messages.privacy_policy') }}</a></li>
-                <li><a href="{{ route('contact') }}">{{ __('messages.contact') }}</a></li>
+                <li><a href="{{ route('sitePolicy') }}">{{ __('message.site_policy') }}</a></li>
+                <li><a href="{{ route('privacyPolicy') }}">{{ __('message.privacy_policy') }}</a></li>
+                <li><a href="{{ route('contact') }}">{{ __('message.contact') }}</a></li>
             </ul>
         </div>
         <div class="footer-logo"><a href="/"><img src="/images/ggroup.svg" alt="株式会社グローバル・コーポレート・コンサルティング"></a>
