caf77e65c8ec22906041c243c37bb71236f49b49
nexus-shirai <shoma.shirai@nexus-jp.co.jp>
2025-10-27 19:33:03 +0900

    TOPのレスポンシブ対応とcssの読み込み設定見直し

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Gss App')</title>
+    <title>@yield('title', 'Gcc App')</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ URL::asset('/css/app.css') }}" rel="stylesheet" type="text/css">
 
     <!-- jQuery UI CSS -->
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 @section('styles')
     @parent
-    <!-- 親のスタイル（app.blade.phpのstylesセクション）を読み込む -->
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/pagesetting.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/modal.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/common.css') }}" />
+    @vite([
+        'resources/css/pagesetting.css',
+        'resources/css/modal.css',
+        'resources/css/common.css',
+    ])
 @endsection
 @include('includes.header')
 @section('content')
