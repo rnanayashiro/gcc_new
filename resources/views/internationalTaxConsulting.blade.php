aac894b8b97fea1e5aa061c65a164abbd8614758
rnanayashiro <rnanayashiro@gmail.com>
2026-01-23 22:33:41 +0900

    fix css

--- resources/views/internationalTaxConsulting.blade.php
+++ resources/views/internationalTaxConsulting.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 @section('styles')
     @parent
-    @vite([
-        'resources/css/pagesetting.css',
-        'resources/css/common.css',
-    ])
+    @vite(['resources/css/pagesetting.css', 'resources/css/common.css'])
 @endsection
 @include('includes.header')
 @section('content')
@@ -13,7 +10,7 @@
             <div class="main-contents">
                 <div class="article-header">
                     <p class="article-category">{{ __('message.service_category') }}</p>
-                    <h1 class="main-title">{{ __('message.company_overview') }}</h1>
+                    <h1 class="main-title">{{ __('message.international_tax_consulting') }}</h1>
                 </div>
                 <div class="image-block"><img src="images/ac02.jpg" alt=""></div>
                 <div class="image-text-block">
